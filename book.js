d28896c17aff345ea6ceefd1d63da6a60a81f55d
ShalomOluwadare <122966416+ShalomOluwadare@users.noreply.github.com>
2023-04-19 22:18:35 +0100

    final version

--- book.js
+++ book.js
@@ -3,6 +3,8 @@ const overlay = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".btn-close");
 const bookButton = document.querySelector(".addBook");
 const table = document.querySelector("table");
+const tbody = document.querySelector("tbody");
+const row1 = document.querySelector("tr");
 const submit = document.querySelector(".submit-btn");
 
 let myLibrary = [];
@@ -25,42 +27,48 @@ function addBookToLibrary() {
   let bookAuthor = document.getElementById("author").value;
   let bookPages = document.getElementById("pages").value;
   let readBook = document.getElementById("read").checked;
-  console.log(readBook);
   let newBook = new book(bookName, bookAuthor, bookPages, readBook);
   myLibrary.push(newBook);
 }
-
-function displayBook() {
-  myLibrary.forEach((e) => {
-    let row = document.createElement("tr");
-    Object.values(e).forEach((text) => {
-      let cell = document.createElement("td");
-      cell.textContent = text;
-      row.appendChild(cell);
-    });
-    let cell3 = document.createElement("td");
-    let cell2 = document.createElement("td");
-    cell3.textContent = "Status";
-    cell2.textContent = "Delete";
-    row.appendChild(cell2);
-    row.appendChild(cell3);
-    table.appendChild(row);
-    cell2.addEventListener("click", () => {
-      table.removeChild(row);
-      myLibrary.splice(myLibrary.indexOf(e), 1);
-      console.log(myLibrary);
-    });
-    cell3.addEventListener("click", () => {
-      e.read = myLibrary[0].read === "yes" ? "no" : "yes";
-      let td = document.querySelectorAll("td");
-      td.item(3).textContent = e.read;
-      console.log(myLibrary[0].read);
-      console.log(e.read);
-      console.log(myLibrary);
-    });
+function displayBook(e) {
+  let row = document.createElement("tr");
+  Object.values(e).forEach((text) => {
+    let cell = document.createElement("td");
+    cell.textContent = text;
+    row.appendChild(cell);
+  });
+  let cell3 = document.createElement("td");
+  let cell2 = document.createElement("td");
+  cell3.textContent = "Status";
+  cell2.textContent = "Delete";
+  row.appendChild(cell2);
+  row.appendChild(cell3);
+  tbody.appendChild(row);
+  cell2.addEventListener("click", () => {
+    deleteRow(e);
+  });
+  cell3.addEventListener("click", () => {
+    changeRead(e);
   });
 }
-
+function changeRead(e) {
+  e.read = e.read === "yes" ? "no" : "yes";
+  myLibrary.forEach(() => {
+    tbody.removeChild(tbody.lastChild);
+  });
+  myLibrary.forEach((i) => {
+    displayBook(i);
+  });
+}
+function deleteRow(e) {
+  myLibrary.forEach(() => {
+    tbody.removeChild(tbody.lastChild);
+  });
+  myLibrary.splice(myLibrary.indexOf(e), 1);
+  myLibrary.forEach((i) => {
+    displayBook(i);
+  });
+}
 function openModal() {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
@@ -72,8 +80,7 @@ function closeModal() {
 closeModalBtn.addEventListener("click", closeModal);
 submit.addEventListener("click", () => {
   addBookToLibrary();
-  console.log(myLibrary);
-  displayBook();
+  displayBook(myLibrary[myLibrary.length - 1]);
   closeModal();
   event.preventDefault();
 });
